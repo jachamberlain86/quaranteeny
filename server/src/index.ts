bb952f089d55db530f76b2712838c69762628f7b
Ross Curry <ross_curry@yahoo.co.uk>
2021-05-08 11:23:00 +0200

    build(/quarantiny): format path fix & typescript style fix on backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,18 +14,19 @@ createConnection()
 
     // register express routes from defined application routes
     Routes.forEach((route) => {
-      (app as any)[route.method](
+      (app as express.Express)[route.method](
         route.route,
-        (req: Request, res: Response, next: Function) => {
+        (req: Request, res: Response, next: express.NextFunction) => {
+          // eslint-disable-next-line
           const result = new (route.controller as any)()[route.action](
             req,
             res,
             next
           );
           if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
+            result.then((outcome) =>
+              outcome !== null && outcome !== undefined
+                ? res.send(outcome)
                 : undefined
             );
           } else if (result !== null && result !== undefined) {
@@ -56,9 +57,10 @@ createConnection()
         age: 24,
       })
     );
-
+    // eslint-disable-next-line no-console
     console.log(
       'Express server has started on port 3000. Open http://localhost:3000/users to see results'
     );
   })
+  // eslint-disable-next-line no-console
   .catch((error) => console.log(error));
