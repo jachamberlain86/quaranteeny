0083001de6956f8b7602845f59e4562dd41ae2f7
romduda <romanduda@gmail.com>
2021-05-10 13:27:31 +0100

    feat(gameSlice.ts): show number of days survived and clock

--- client/src/components/DayCounter/DayCounter.component.tsx
+++ client/src/components/DayCounter/DayCounter.component.tsx
@@ -1,11 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import './DayCounter.styles.css';
+import moment from 'moment';
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import {
+  selectClockTime,
+  selectStartTime,
+  startClock,
+} from '../../features/game/gameSlice';
 
 const DayCounter: FC = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(startClock());
+  }, [dispatch]);
+  const startTime = useAppSelector(selectStartTime);
+  const currClockTime = useAppSelector(selectClockTime);
+
+  const timeSinceStart = moment(startTime).from(currClockTime, true);
+  const date = moment(currClockTime).format('ll');
+  const clock = moment(currClockTime).format('h:mm a');
   return (
     <div className="conNum">
-      <div className="numHeader">Number Of Days</div>
-      <div className="numOfDays"> 0? </div>
+      <div className="numHeader">
+        <p>Survived lockdown for:</p>
+      </div>
+      <div className="numOfDays">
+        <h2>{timeSinceStart}</h2>
+      </div>
+      <div className="date"> {date} </div>
+      <div className="clock"> {clock} </div>
     </div>
   );
 };
--- client/src/features/game/gameSlice.ts
+++ client/src/features/game/gameSlice.ts
@@ -1,13 +1,19 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../../app/store';
+import { RootState, AppThunk } from '../../app/store';
 
 export interface GameState {
+  // gameStatus: 'alive' | 'lost';
   gameSpeed: number;
+  currClockTime: number;
+  startTime: number;
 }
 
 const initialState: GameState = {
-  gameSpeed: 100,
+  // gameStatus: 'alive',
+  gameSpeed: 10000,
+  startTime: Date.now(),
+  currClockTime: Date.now(),
 };
 
 export const gameSlice = createSlice({
@@ -17,12 +23,51 @@ export const gameSlice = createSlice({
     changeGameSpeed: (state, action: PayloadAction<number>) => {
       state.gameSpeed = action.payload;
     },
+    updateClockTime: (
+      state,
+      action: PayloadAction<{ gameSpeed: number; prevClockTime: number }>
+    ) => {
+      // `diff` is the time elapsed since last update to the clock
+      const diff = Date.now() - action.payload.prevClockTime;
+      state.currClockTime =
+        action.payload.prevClockTime + diff * action.payload.gameSpeed;
+    },
+    // setGameStatus: (state, action: PayloadAction<'alive' | 'lost'>) => {
+    //   state.gameStatus = action.payload;
+    // },
   },
 });
 
-export const { changeGameSpeed } = gameSlice.actions;
+export const {
+  changeGameSpeed,
+  updateClockTime,
+  // setGameStatus,
+} = gameSlice.actions;
+
+// export const selectGameStatus = (state: RootState): string =>
+//   state.game.gameStatus;
 
 export const selectGameSpeed = (state: RootState): number =>
   state.game.gameSpeed;
+
+export const selectStartTime = (state: RootState): number =>
+  state.game.startTime;
+
+export const selectClockTime = (state: RootState): number =>
+  state.game.currClockTime;
+
+export const startClock = (): AppThunk => (dispatch, getState) => {
+  const fiveSeconds = 1000 * 5;
+  const prevClockTime = selectClockTime(getState());
+  const gameSpeed = selectGameSpeed(getState());
+  // const gameStatus = selectGameStatus(getState());
+  const clockInterval = setInterval(() => {
+    // if (gameStatus === 'lost') {
+    // clearInterval(clockInterval);
+    // } else if (gameStatus === 'alive') {
+    dispatch(updateClockTime({ gameSpeed, prevClockTime }));
+    // }
+  }, fiveSeconds);
+};
 
 export default gameSlice.reducer;
