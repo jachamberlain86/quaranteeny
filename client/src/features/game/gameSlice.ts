7e74cde2cf067445071256cae12593268940700c
romduda <romanduda@gmail.com>
2021-05-10 13:39:42 +0100

    refactor(gameSlice.ts): remove commented out code

--- client/src/features/game/gameSlice.ts
+++ client/src/features/game/gameSlice.ts
@@ -3,14 +3,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../../app/store';
 
 export interface GameState {
-  // gameStatus: 'alive' | 'lost';
   gameSpeed: number;
   currClockTime: number;
   startTime: number;
 }
 
 const initialState: GameState = {
-  // gameStatus: 'alive',
   gameSpeed: 100,
   startTime: Date.now(),
   currClockTime: Date.now(),
@@ -32,20 +30,10 @@ export const gameSlice = createSlice({
       state.currClockTime =
         action.payload.prevClockTime + diff * action.payload.gameSpeed;
     },
-    // setGameStatus: (state, action: PayloadAction<'alive' | 'lost'>) => {
-    //   state.gameStatus = action.payload;
-    // },
   },
 });
 
-export const {
-  changeGameSpeed,
-  updateClockTime,
-  // setGameStatus,
-} = gameSlice.actions;
-
-// export const selectGameStatus = (state: RootState): string =>
-//   state.game.gameStatus;
+export const { changeGameSpeed, updateClockTime } = gameSlice.actions;
 
 export const selectGameSpeed = (state: RootState): number =>
   state.game.gameSpeed;
@@ -60,13 +48,8 @@ export const startClock = (): AppThunk => (dispatch, getState) => {
   const fiveSeconds = 1000 * 5;
   const prevClockTime = selectClockTime(getState());
   const gameSpeed = selectGameSpeed(getState());
-  // const gameStatus = selectGameStatus(getState());
   const clockInterval = setInterval(() => {
-    // if (gameStatus === 'lost') {
-    // clearInterval(clockInterval);
-    // } else if (gameStatus === 'alive') {
     dispatch(updateClockTime({ gameSpeed, prevClockTime }));
-    // }
   }, fiveSeconds);
 };
 
