4e978f6a5c879372312f86e2b1b50ac3816164b6
Ross Curry <ross_curry@yahoo.co.uk>
2021-05-11 16:53:09 +0200

    feat(client): interaction progress bar inserted over sprite with nes css stylings

---
+++ client/src/components/ProgressBar/ProgressBar.component.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import './ProgressBar.styles.css';
+import { useAppSelector } from '../../app/hooks';
+
+const ProgressBar = (): JSX.Element => {
+  const interactionProgress: number | null = useAppSelector(
+    (state) => state.sprite.interactionProgress
+  );
+
+  const displayProgressBar = (): JSX.Element => (
+    <progress
+      className="nes-progress is-success"
+      value={interactionProgress || 0.1}
+      max={100}
+    />
+  );
+
+  return (
+    <div className="interaction-progress">
+      {interactionProgress ? displayProgressBar() : null}
+    </div>
+  );
+};
+
+export default ProgressBar;
