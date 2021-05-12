5769f89983561f7889c85b366c3aa1267f2004d2
Ross Curry <ross_curry@yahoo.co.uk>
2021-05-12 21:30:49 +0200

    feat(client): clock stops when game over is true

--- client/src/components/DayCounter/DayCounter.component.tsx
+++ client/src/components/DayCounter/DayCounter.component.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './DayCounter.styles.css';
 import moment from 'moment';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
@@ -12,7 +12,8 @@ import { selectUserStatus } from '../../features/user/userSlice';
 const DayCounter = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const userLoadingStatus = useAppSelector(selectUserStatus);
-  const gameOverState = useAppSelector((state) => state.game);
+  const { gameOver } = useAppSelector((state) => state.game);
+  const [timeOfDeath, setTimeOfDeath] = useState(0);
   useEffect(() => {
     if (userLoadingStatus === 'userLoaded') {
       startClock();
@@ -20,20 +21,29 @@ const DayCounter = (): JSX.Element => {
   }, [dispatch, userLoadingStatus]);
   const startTime = useAppSelector(selectStartTime);
   const currClockTime = useAppSelector(selectClockTimeInGame);
+  useEffect(() => {
+    if (gameOver) {
+      const momentOfDeath = currClockTime;
+      setTimeOfDeath(momentOfDeath);
+    }
+  }, [gameOver]);
 
   const timeSinceStart = moment(startTime).from(currClockTime, true);
   const date = moment(currClockTime).format('ll');
   const clock = moment(currClockTime).format('h:mm a');
+  const timeLasted = moment(startTime).from(timeOfDeath, true);
+  const dateDied = moment(timeOfDeath).format('ll');
+  const clockStopped = moment(timeOfDeath).format('h:mm a');
   return (
     <div className="conNum">
       <div className="numHeader">
         <p>Survived lockdown for:</p>
       </div>
       <div className="numOfDays">
-        <h2>{timeSinceStart}</h2>
+        <h2>{gameOver ? timeLasted : timeSinceStart}</h2>
       </div>
-      <div className="date"> {date} </div>
-      <div className="clock"> {clock} </div>
+      <div className="date"> {gameOver ? dateDied : date} </div>
+      <div className="clock"> {gameOver ? clockStopped : clock} </div>
     </div>
   );
 };
