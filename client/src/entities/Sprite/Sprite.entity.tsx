a208c4598d4f9f7ba76dabea6c511942a26a360b
romduda <romanduda@gmail.com>
2021-05-12 14:24:18 +0100

    feat(sprite.entity.tsx): start setInterval functions only after user data from db has loaded

--- client/src/entities/Sprite/Sprite.entity.tsx
+++ client/src/entities/Sprite/Sprite.entity.tsx
@@ -1,18 +1,23 @@
 import React, { FC, useEffect } from 'react';
+import { useAppSelector } from '../../app/hooks';
 import {
   checkLoseStates,
   checkConditionsState,
 } from '../../helpers/sprite.helper';
 import { checkMeterStates } from '../../helpers/meters.helper';
+import { selectUserStatus } from '../../features/user/userSlice';
 
 import './Sprite.styles.css';
 
 const Sprite: FC = () => {
+  const userLoadingStatus = useAppSelector(selectUserStatus);
   useEffect(() => {
-    checkMeterStates();
-    checkConditionsState();
-    checkLoseStates();
-  }, []);
+    if (userLoadingStatus === 'userLoaded') {
+      checkMeterStates();
+      checkConditionsState();
+      checkLoseStates();
+    }
+  }, [userLoadingStatus]);
   return <div className="sprite" />;
 };
 
