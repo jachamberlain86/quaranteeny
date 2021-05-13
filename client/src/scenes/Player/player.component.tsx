17448bb4af6529a88393f19bfb8ef8879da9f3ca
Ro Bullingdon <176murrayst@gmail.com>
2021-05-13 16:53:21 +0100

    ok

--- client/src/scenes/Player/player.component.tsx
+++ client/src/scenes/Player/player.component.tsx
@@ -5,19 +5,56 @@ import { triggerPlaceAt } from '../../helpers/player.helper';
 
 const Player: FC = () => {
   const player = useAppSelector((store) => store.character);
+
+  const [currentFrameTime, setCurrentFrameTime] = useState(0);
   const [currentSecond, setCurrentSecond] = useState(0);
   const [frameCount, setframeCount] = useState(0);
   const [framesLastSecond, setframesLastSecond] = useState(0);
   const [lastFrameTime, setlastFrameTime] = useState(0);
   const [moving, setMoving] = useState(false);
 
-  useEffect(() => {
     window.addEventListener('keydown', function (e) {
       if (e.key === 'ArrowDown') {
         triggerPlaceAt(1, 1, 'left');
+  const downKey = useKeyPress('ArrowDown');
+  const leftKey = useKeyPress('ArrowLeft');
+  const rightKey = useKeyPress('ArrowRight');
+
+  function checkIndex(x: number, y: number): number {
+    return y * cols + x;
+  }
+
+  function drawGame(): any {
+    setCurrentFrameTime(Date.now());
+
+    const sec = Math.floor(Date.now() / 1000);
+    if (sec !== currentSecond) {
+      setCurrentSecond(sec);
+      setFramesLastSecond(frameCount);
+      setFrameCount(1);
+    } else {
+      setFrameCount(frameCount + 1);
+    }
+
+    if (!player.isMoving) {
+      const timer: any = null;
+      if (downKey === true) {
+        // timer = setInterval(() => {
+        if (
+          player?.tileFrom[1] < cols - 1 &&
+          layers[0][checkIndex(player.tileFrom[0], player.tileFrom[1] + 1)] ===
+            1
+        ) {
+          console.log('player moves down');
+          console.log(downKey);
+          triggerMove('down');
+          // triggerProcessMovement(currentSecond);
+        }
+        // }, 500);
+        // } else {
+        //   clearInterval(timer);
       }
       if (e.key === 'ArrowUp') {
-        console.log('up');
       }
       if (e.key === 'ArrowLeft') {
         console.log('left');
