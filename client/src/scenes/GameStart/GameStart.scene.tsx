e5058135145b6d15dc0fa980e12164f9aa462e3b
Ross Curry <ross_curry@yahoo.co.uk>
2021-05-13 17:18:41 +0200

    feat(client): choose speed buttons added

--- client/src/scenes/GameStart/GameStart.scene.tsx
+++ client/src/scenes/GameStart/GameStart.scene.tsx
@@ -3,9 +3,18 @@ import './GameStart.styles.css';
 import { useHistory } from 'react-router-dom';
 import { setUserName } from '../../features/user/userSlice';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { changeGameSpeed } from '../../features/game/gameSlice';
+
+interface initialState {
+  name: string;
+}
+const initialState = {
+  name: 'showStartBtn',
+};
 
 const GameStart = (): JSX.Element => {
-  const [animate, setAnimate] = useState(false);
+  const animationSpeed = 300;
+  const [animate, setAnimate] = useState(initialState);
   const [nameInput, setNameInput] = useState('');
   const history = useHistory();
   const dispatch = useAppDispatch();
@@ -14,10 +23,17 @@ const GameStart = (): JSX.Element => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    const target = e.target as HTMLFormElement;
+    target.classList.add('slideOutLeft');
     dispatch(setUserName(nameInput));
+    // setTimeout(() => {
+    //   history.push('/start');
+    // }, 500);
     setTimeout(() => {
-      history.push('/start');
-    }, 500);
+      setAnimate({ name: 'showChooseGameSpeed' });
+      setNameInput('');
+      target.classList.add('displayOff');
+    }, animationSpeed);
   };
 
   const handleInput = (e: React.FormEvent<HTMLInputElement>): void => {
@@ -25,31 +41,57 @@ const GameStart = (): JSX.Element => {
     setNameInput(input);
   };
 
-  const startGameBtn: JSX.Element = (
-    <button
-      type="button"
-      className={
-        animate
-          ? 'nes-btn is-error slideOut start-btn'
-          : 'nes-btn is-error start-btn'
-      }
-      onClick={(e): void => {
-        setAnimate(!animate);
-        setTimeout(() => {
-          const cssStyle = e.target as Element;
-          cssStyle.classList.add('displayOff');
-        }, 600);
-      }}
-    >
-      Start
-    </button>
-  );
+  const handleStart = (e: React.FormEvent<HTMLButtonElement>): void => {
+    const cssStyle = e.target as Element;
+    console.log('cssStyle', cssStyle);
+    cssStyle.classList.add('slideOutLeft');
+    if (userId) {
+      // TODO finish logic to show return user here
+      setTimeout(() => {
+        cssStyle.classList.add('displayOff');
+        setAnimate({ name: 'showReturnUser' });
+      }, animationSpeed);
+    } else {
+      setTimeout(() => {
+        cssStyle.classList.add('displayOff');
+        setAnimate({ name: 'showForm' });
+      }, animationSpeed);
+    }
+  };
+
+  const handleChooseSpeed = (e: React.FormEvent<HTMLButtonElement>): void => {
+    const { id } = e.target as Element;
+    if (id === 'realTime') {
+      changeGameSpeed(1);
+    } else if (id === 'oneMinIsOneHour') {
+      changeGameSpeed(60);
+    } else if (id === 'oneMinIsOneDay') {
+      changeGameSpeed(1440);
+    }
+    setTimeout(() => {
+      console.log('change screens');
+    });
+  };
+
+  const renderStartBtn = (): JSX.Element => {
+    return (
+      <button
+        type="button"
+        className="nes-btn is-error start-btn"
+        onClick={handleStart}
+      >
+        Start
+      </button>
+    );
+  };
 
   const renderNewUserForm = (): JSX.Element => {
     return (
       <div>
         <form
-          className={animate ? 'form slideIn' : 'displayOff'}
+          className={
+            animate.name === 'showForm' ? 'form slideIn' : 'displayOff'
+          }
           onSubmit={handleSubmit}
           autoComplete="off"
         >
@@ -65,7 +107,11 @@ const GameStart = (): JSX.Element => {
               onChange={handleInput}
             />
           </label>
-          <button type="submit" className="nes-btn is-error submit-btn">
+          <button
+            type="submit"
+            className="nes-btn is-error submit-btn"
+            disabled={!nameInput}
+          >
             Play
           </button>
         </form>
@@ -75,12 +121,26 @@ const GameStart = (): JSX.Element => {
 
   const renderReturnUser = (): JSX.Element => {
     return (
-      <div className={animate ? 'return-user-container' : 'displayOff'}>
-        <div className={animate ? 'slideInFromLeft' : 'displayOff'}>
+      <div
+        className={
+          animate.name === 'showReturnUser'
+            ? 'return-user-container'
+            : 'displayOff'
+        }
+      >
+        <div
+          className={
+            animate.name === 'showReturnUser' ? 'slideInFromLeft' : 'displayOff'
+          }
+        >
           <h3 className="rtn-title">Hey {userName}, welcome back</h3>
         </div>
         <div
-          className={animate ? 'return-btn-container slideIn' : 'displayOff'}
+          className={
+            animate.name === 'showReturnUser'
+              ? 'return-btn-container slideIn'
+              : 'displayOff'
+          }
         >
           {gameOver ? (
             <button type="button" className="nes-btn is-error rtn-btn">
@@ -105,6 +165,42 @@ const GameStart = (): JSX.Element => {
     );
   };
 
+  const renderChooseSpeed = (): JSX.Element => {
+    return (
+      <div>
+        <div className="choose-speed-title">
+          <h3>Choose game speed</h3>
+        </div>
+        <div className="speed-btn-container">
+          <button
+            type="button"
+            className="nes-btn speed-btn"
+            id="realTime"
+            onClick={handleChooseSpeed}
+          >
+            Real time
+          </button>
+          <button
+            type="button"
+            className="nes-btn speed-btn is-warning"
+            id="oneMinIsOneHour"
+            onClick={handleChooseSpeed}
+          >
+            1min = 1hour
+          </button>
+          <button
+            type="button"
+            className="nes-btn speed-btn is-error"
+            id="oneMinIsOneDay"
+            onClick={handleChooseSpeed}
+          >
+            1min = 1day
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="game-start-page">
       <div className="start-page-container">
@@ -112,8 +208,9 @@ const GameStart = (): JSX.Element => {
           <h1>Quarantiny</h1>
         </div>
         <div className="bottom-row">
-          {startGameBtn}
+          {animate.name === 'showStartBtn' ? renderStartBtn() : null}
           {userId && !gameOver ? renderReturnUser() : renderNewUserForm()}
+          {animate.name === 'showChooseGameSpeed' ? renderChooseSpeed() : null}
         </div>
       </div>
     </div>
