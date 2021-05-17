8a00c7afacf7128810468b8a7c9746bc9ba67072
Ro Bullingdon <176murrayst@gmail.com>
2021-05-17 15:41:36 +0100

    adding directions to sprite animations

--- client/src/scenes/Player/player.component.tsx
+++ client/src/scenes/Player/player.component.tsx
@@ -17,53 +17,59 @@ const Player = (): JSX.Element => {
     setSpriteAnimationDirection,
   ] = useState<string>('walkingUp');
 
-  const { walkingD } = animationDirectory;
+  const { walkingD, walkingU, walkingL, walkingR } = animationDirectory;
 
   const animations = {
     idle: [
-      2,
-      10,
-      tileSize,
-      tileSize,
-      76,
-      138,
-      tileSize,
-      tileSize,
-      346,
-      138,
-      tileSize,
-      tileSize,
-    ],
-    walkingUp: [
-      2,
-      2,
-      tileSize,
-      tileSize,
-      71,
-      2,
-      tileSize,
-      tileSize,
-      146,
-      2,
-      tileSize,
-      tileSize,
-      226,
-      2,
-      tileSize,
-      tileSize,
+      walkingD[0].x,
+      walkingD[0].y,
+      walkingD[0].width,
+      walkingD[0].height,
+      walkingD[1].x,
+      walkingD[1].y,
+      walkingD[1].width,
+      walkingD[1].height,
     ],
     walkingDown: [
       walkingD[0].x,
       walkingD[0].y,
-      walkingD[0].pixWidth,
-      walkingD[0].pixHeight,
+      walkingD[0].width,
+      walkingD[0].height,
       walkingD[1].x,
       walkingD[1].y,
-      walkingD[1].pixWidth,
-      walkingD[1].pixHeight,
+      walkingD[1].width,
+      walkingD[1].height,
     ],
-    walkingLeft: [2, 138, 74, 122, 76, 138, 84, 122, 346, 138, 120, 122],
-    walkingRight: [2, 138, 74, 122, 76, 138, 84, 122, 346, 138, 120, 122],
+    walkingUp: [
+      walkingU[0].x,
+      walkingU[0].y,
+      walkingU[0].width,
+      walkingU[0].height,
+      walkingU[1].x,
+      walkingU[1].y,
+      walkingU[1].width,
+      walkingU[1].height,
+    ],
+    walkingLeft: [
+      walkingL[0].x,
+      walkingL[0].y,
+      walkingL[0].width,
+      walkingL[0].height,
+      walkingL[1].x,
+      walkingL[1].y,
+      walkingL[1].width,
+      walkingL[1].height,
+    ],
+    walkingRight: [
+      walkingR[0].x,
+      walkingR[0].y,
+      walkingR[0].width,
+      walkingR[0].height,
+      walkingR[1].x,
+      walkingR[1].y,
+      walkingR[1].width,
+      walkingR[1].height,
+    ],
   };
 
   useEffect(() => {
@@ -81,7 +87,7 @@ const Player = (): JSX.Element => {
     const ref = spriteRef.current;
     ref.to({
       x: character.pixelLocation[0],
-      y: character.pixelLocation[1] - 2 * tileSize,
+      y: character.pixelLocation[1] - tileSize / 2,
       duration: character.delay / 1000,
     });
     spriteRef.current.start();
