620789839d44f6f27d5e07c0a5d0effd32912e9d
Ro Bullingdon <176murrayst@gmail.com>
2021-05-19 16:46:36 +0100

    style: format new game page

--- client/src/scenes/NewGameScreen/NewGameScreen.scene.tsx
+++ client/src/scenes/NewGameScreen/NewGameScreen.scene.tsx
@@ -28,56 +28,66 @@ import { setCurrentSong } from '../../features/music/musicSlice';
 
 const NewGameScreen = (): JSX.Element => {
   return (
-    <div className="max-width-container">
-      <div className="new-game-page-content">
-        <div className="difficulty-radio-buttons">
-          <div className="diff-radio-button" id="easy">
-            easy
-            <input type="radio" />
-          </div>
-          <div className="diff-radio-button" id="middle">
-            medium
-            <input type="radio" />
-          </div>
-          <div className="diff-radio-button" id="hard">
-            hard
-            <input type="radio" />
-          </div>
-        </div>
-        <div className="direction-key-instructions">
-          <div className="key-arrow" id="up-arrow" />
-          <div className="letter-instruction" id="w">
-            W
-          </div>
-          <div className="bottom-row-of-direction-buttons">
-            <div className="key-arrow" id="left-arrow" />
+    <div className="game-container">
+      <div className="max-width-container">
+        <div className="new-game-page-content">
+          <div className="difficulty-radio-buttons">
+            <div className="diff-radio-button-space" id="easy">
+              <h1>easy</h1>
+              <p>1 Real Minute = 1 Quaranteeny Minute</p>
+              <input type="radio" className="diff-radio" />
+            </div>
+            <div className="diff-radio-button-space" id="middle">
+              <h1>medium</h1>
+              <p>1 Real Minute = 1 Quaranteeny Hour</p>
 
-            <div className="letter-instruction" id="a">
-              A
+              <input type="radio" className="diff-radio" />
             </div>
-            <div className="letter-instruction" id="s">
-              S
-            </div>
-            <div className="letter-instruction" id="d">
-              D
-            </div>
-            <div className="key-arrow" id="right-arrow" />
-          </div>
-          <div className="key-arrow" id="down-arrow" />
-        </div>
-        <div className="k-and-l-key-instructions">
-          <div className="letter-instruction" id="k">
-            K
-          </div>
-          <div className="letter-instruction" id="l">
-            L
-          </div>
-        </div>
-      </div>
-      <div> press enter to start </div>
+            <div className="diff-radio-button-space" id="hard">
+              <h1> hard</h1>
+              <p>1 Real Minute = 1 Quaranteeny Day</p>
 
-      <div className="graphic-room-items-border-bar">
-        the plants and stuff will go here
+              <input type="radio" className="diff-radio" />
+            </div>
+          </div>
+          <div className="direction-key-instructions">
+            <div className="key-arrow" id="up-arrow" />
+            <div className="letter-instruction" id="w">
+              W
+            </div>
+            <div className="bottom-row-of-direction-buttons">
+              <div className="key-arrow" id="left-arrow" />
+
+              <div className="letter-instruction" id="a">
+                A
+              </div>
+              <div className="letter-instruction" id="s">
+                S
+              </div>
+              <div className="letter-instruction" id="d">
+                D
+              </div>
+              <div className="key-arrow" id="right-arrow" />
+            </div>
+            <div className="key-arrow" id="down-arrow" />
+            <p> Use the W, A, S, and D keys to move around the screen</p>
+          </div>
+          <div className="direction-key-instructions">
+            <div className="k-and-l-key-instructions">
+              <div className="letter-instruction" id="k">
+                K
+              </div>
+              <div className="letter-instruction" id="l">
+                L
+              </div>
+            </div>
+            <p>
+              {' '}
+              Your Quaranteeny will tell you when they need something. Use K and
+              L to interact with items in their flat
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
